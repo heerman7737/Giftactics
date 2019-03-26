b68228a9e4b643333fe63a51bde203a991ec6524
Giang <giang.to.7737@gmail.com>
2019-03-26 16:34:04 -0700

    Final commit(too hard)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,27 +8,41 @@ function printBtn() {
        let btn = document.createElement("button");
        let t = document.createTextNode(gif.topics[i]);
        btn.setAttribute("class","btn");
-       btn.appendChild(t);
+       btn.appendChild(t);     
        document.body.appendChild(btn);
+       var mytopic=gif.topics[i]
+       // Do not understand why mytopic does not get the value from gif.topics[i]
+       btn.onclick = function(mytopic) {
+        
+        fetch(`https://api.giphy.com/v1/gifs/search?api_key=QpGy8tofOxdRNC5gq0pmrLjq6jadIHbx&q=${mytopic}&limit=10&offset=0&rating=R&lang=en
+        `)
+        .then(mytopic=>mytopic.json())
+        .then(gifs=>{
+          document.querySelector('#gifDiv').innerHTML = ''
+          gifs.data.forEach(element => {
+            let ele=document.createElement('img')
+            ele.setAttribute('src',element.images.original.url)
+            document.getElementById('gifDiv').append(ele)
+            
+          });
+        })
+      }
+      
     }
 }
+
+  
+
 function search(){
+  // A search function that take input value from <input>
+  // Request from giphy
+  // Event create <button class="btn">
 
 }
-function pressBtn () {
-  
-    fetch(`https://api.giphy.com/v1/gifs/search?api_key=QpGy8tofOxdRNC5gq0pmrLjq6jadIHbx&q=${gif.topics[i]}&limit=30&offset=0&rating=R&lang=en
-`)
-.then(sound=>sound.json())
-.then(gifs=>{
-  document.querySelector('#gifDiv').innerHTML = ''
-  gifs.data.forEach(element => {
-    let ele=document.createElement('img')
-    ele.setAttribute('src',element.images.original.url)
-    document.getElementById('gifDiv').append(ele)
-    
-  });
-})
-  
-  };
+function clickStill(){
+  //Event click change source from images.original to images.original_still
+}
+
+
+
 printBtn();
