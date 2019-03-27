6ad7e8d7ab35cd825f2ed89d17a9f334d51b3bd4
Giang <giang.to.7737@gmail.com>
2019-03-26 20:11:23 -0700

    nothing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ let gif={
     topics:["cat","dog","fox","dragon","supercute","mars","kingdom"],
     search:""
 }
-
+let mytopic=gif.topics[0]
 function printBtn() {
     for (let i = 0; i < gif.topics.length; i++) {
        let btn = document.createElement("button");
@@ -10,9 +10,9 @@ function printBtn() {
        btn.setAttribute("class","btn");
        btn.appendChild(t);     
        document.body.appendChild(btn);
-       var mytopic=gif.topics[i]
+
        // Do not understand why mytopic does not get the value from gif.topics[i]
-       btn.onclick = function(mytopic) {
+       btn.onclick = function() {
         
         fetch(`https://api.giphy.com/v1/gifs/search?api_key=QpGy8tofOxdRNC5gq0pmrLjq6jadIHbx&q=${mytopic}&limit=10&offset=0&rating=R&lang=en
         `)
